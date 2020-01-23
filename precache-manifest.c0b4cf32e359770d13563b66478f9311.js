ad896d7110fbce49cdb24f46436863a8a316dc0f
Gongxia Chen <chengongxia1990@gmail.com>
2020-01-22 23:43:33 -0500

    Updates

---
+++ precache-manifest.c0b4cf32e359770d13563b66478f9311.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d91048fb522ffcab0a7920ba8b968666",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f06542b92cb8a50629f",
+    "url": "/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "7916381aaa65204853c9",
+    "url": "/static/js/2.4f819a3f.chunk.js"
+  },
+  {
+    "revision": "efe80d506e138c343a03a1541511a2f3",
+    "url": "/static/js/2.4f819a3f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3f06542b92cb8a50629f",
+    "url": "/static/js/main.0ad90086.chunk.js"
+  },
+  {
+    "revision": "7c67f03dac9b69ca3528",
+    "url": "/static/js/runtime-main.43582363.js"
+  }
+]);
