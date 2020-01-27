a7a4cce28684974eeea7f8e9de06c692a740c230
Gongxia Chen <chengongxia1990@gmail.com>
2020-01-27 02:42:32 -0500

    implement the basic counter feature

--- src/components/Donut.js
+++ src/components/Donut.js
@@ -10,7 +10,24 @@ const data = [
 const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
 
 export default class Donut extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timeRemain: 60
+    };
+  }
+
+  componentDidMount = () => {
+    setInterval(() =>
+      this.setState({
+        timeRemain: this.state.timeRemain - 1
+      }), 1000
+    );
+  };
+
   render() {
+    const endAngle = 180 - this.state.timeRemain * 6;
+    console.log(endAngle);
     return (
       <PieChart width={400} height={400} onMouseEnter={this.onPieEnter}>
         <Pie
@@ -18,12 +35,13 @@ export default class Donut extends React.Component {
           cx={200}
           cy={200}
           startAngle={180}
-          endAngle={-180}
+          endAngle={endAngle}
           innerRadius={120}
           outerRadius={160}
           fill="#8884d8"
           paddingAngle={2}
           dataKey="value"
+          animationDuration={600}
         >
           {data.map((entry, index) => (
             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
